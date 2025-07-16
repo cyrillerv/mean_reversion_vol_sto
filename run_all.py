f37f553e06c58fb9ca9c9318e57f676a0959a140
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-16 11:20:32 +0100

    add heston vol in run_all file + add the global function to print graphs

--- run_all.py
+++ run_all.py
@@ -2,14 +2,17 @@ from src.data_loader import load_data
 from src.vol_model import estimate_volatility
 from src.mean_reversion import generate_signals
 from src.backtest import backtest_strategy
-from src.plot_results import plot_equity_curve
+from src.plot_results import plot_results
+
+from src.heston_model import heston_vol
 
 def main():
     data = load_data()
-    data = estimate_volatility(data)
+    data = heston_vol(data)
+    # data = estimate_volatility(data) # Simple rolling vol
     data = generate_signals(data)
     data = backtest_strategy(data)
-    plot_equity_curve(data)
+    plot_results(data)
 
 if __name__ == "__main__":
     main()
